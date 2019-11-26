d9786d7c229b1184f9a0ca12dd65bda9f78648bc
Aili Gong <aili.hgong@gmail.com>
2019-11-26 02:19:56 +0000

    Commented bubbleSort

--- BubbleSort.java
+++ BubbleSort.java
@@ -5,7 +5,7 @@ public class BubbleSort {
     public static void bubbleSort(int[] arr, int n) {
         int i, j, temp;
         boolean swapped;
-        for (i = 0; i < n - 1; i++) {  // largest i is n-2
+        for (i = 0; i < n - 1; i++) {  // largest i is n-2, or else (n-i-1) would be 0 and the inside loop would not run anyways
             swapped = false;
             for (j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
